b2d180ae73ab0b9540c7cad6ec417a871895d52a
GinoOff <matteo07.carpentieri@gmail.com>
2025-01-27 07:46:10 +0100

    Creazione del file del logout

--- nodeServer/routes/authRoutes.js
+++ nodeServer/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const {login} = require('../controllers/authController')
+const login = require('../controllers/authController')
+const logout = require('../controllers/logout')
 
 const router = express.Router()
 
 router.post('/login', login)
+router.post('/logout', logout)
 
 module.exports = router
