c700aefa055d8fbe57adfe6c1588aa61223f179b
GinoOff <matteo07.carpentieri@gmail.com>
2025-02-04 21:11:25 +0100

    Prima parte frontend dell'header

---
+++ Frontend/components/header/headerScript.js
@@ -0,0 +1,57 @@
+let headerDD = false;
+let respheaderDD = false;
+const header = document.querySelector("header");
+const respButton = document.getElementById("respMenuButton");
+const respBlock = document.getElementById("respBlock1");
+const respbuttonText = document.getElementById("respButtonText");
+const dotH = document.getElementById('dotH');
+
+function handleDesktopBehavior() {
+    header.addEventListener('click', toggleHeader);
+    respButton.removeEventListener('click', toggleMobileMenu);
+}
+
+function handleMobileBehavior() {
+    header.removeEventListener('click', toggleHeader);
+    respButton.addEventListener('click', toggleMobileMenu);
+    respBlock.style.transform = 'translateY(10%)';
+    respBlock.style.opacity = '0';
+    respheaderDD = false;
+    respbuttonText.textContent = 'MENU';
+}
+
+function toggleHeader() {
+    headerDD = !headerDD;
+    header.style.transform = headerDD ?  'translateY(0)' : 'translateY(-100px)';
+}
+
+function toggleMobileMenu() {
+    respheaderDD = !respheaderDD;
+    respBlock.style.transform = respheaderDD ? 'translateY(0)' : 'translateY(10%)';
+    respBlock.style.opacity = respheaderDD ? '1' : '0';
+    respbuttonText.textContent = respheaderDD ? 'CHIUDI' : 'MENU';
+}
+
+function handleResponsive() {
+    if (window.innerWidth > 999) {
+        handleDesktopBehavior();
+    } else {
+        headerDD = true;
+        header.style.transform = 'translateY(0)';
+        handleMobileBehavior();
+    }
+}
+
+handleResponsive();
+function debounce(func, timeout = 100) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), timeout);
+    };
+}
+
+window.addEventListener('resize', debounce(handleResponsive));
+
+const fileName = window.location.pathname.split('/').pop();
+dotH.style.display = fileName === 'index.html' ? 'block' : 'none';
---
+++ Frontend/routes/componentLoader.js
@@ -0,0 +1,50 @@
+function loadCss(cssPath) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssPath;
+    document.head.appendChild(link);
+}
+
+function loadJs(jsPath) {
+    const script = document.createElement('script');
+    script.src = jsPath;
+    script.async = true; // Carica lo script in modo asincrono
+    document.body.appendChild(script);
+}
+
+// Funzione per caricare i componenti header e footer
+function loadComponents(selector, filepath, cssPath = null, jsPath = null) {
+    fetch(filepath).then(response => {
+        if(!response.ok) {
+            throw new Error(`Errore nel caricamento del ${filepath}`);
+        }
+        return response.text();
+    }).then(content => {
+        document.querySelector(selector).innerHTML = content;
+
+        if(cssPath) {
+            loadCss(cssPath);
+        }
+
+        if(jsPath) {
+            loadJs(jsPath);
+        }
+
+    }).catch(error => console.error(error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadComponents(
+        "#header",
+        "/BiteLine/Frontend/components/Header/header.html",
+        "/BiteLine/Frontend/assets/css/styleComponent/header.css",
+        "/BiteLine/Frontend/components/Header/headerScript.js"  // Nuovo parametro JS
+    );
+
+    loadComponents(
+        "#footer",
+        "/BiteLine/Frontend/components/Footer/footer.html",
+        "/BiteLine/Frontend/assets/css/styleComponent/footer.css",
+        "/BiteLine/Frontend/components/Footer/footer.js"  // Nuovo parametro JS
+    );
+});
