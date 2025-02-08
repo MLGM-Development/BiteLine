8abaf219d9e0aebd4af076549016f4eeaea1de2e
GinoOff <matteo07.carpentieri@gmail.com>
2025-02-08 07:07:44 +0100

    Homepage mockup switcher

--- Frontend/assets/script/homeScript.js
+++ Frontend/assets/script/homeScript.js
@@ -1,6 +1,17 @@
 let titleHome = document.getElementById("titleHome");
 let preTitle = document.querySelector(".preTitle");
 
+let isMock1 = true;
+let isMock2 = false;
+let isMock3 = false;
+let mock1 = document.getElementById("mock1");
+let mock2 = document.getElementById("mock2");
+let mock3 = document.getElementById("mock3");
+
+let rest1 = document.getElementById("sponsorEl1");
+let rest2 = document.getElementById("sponsorEl2");
+let rest3 = document.getElementById("sponsorEl3");
+
 window.onload = function () {
     setTimeout(function() {
         titleHome.style.fontSize = "";
@@ -27,3 +38,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     observer.observe(video);
 });
+
+rest1.addEventListener("click", function() {
+    if (!isMock1) {
+        mock1.style.display = "block";
+        mock2.style.display = "none";
+        mock3.style.display = "none";
+        isMock1 = true;
+        isMock2 = false;
+        isMock3 = false;
+    }
+})
+
+rest2.addEventListener("click", function() {
+    if (!isMock2) {
+        mock1.style.display = "none";
+        mock2.style.display = "block";
+        mock3.style.display = "none";
+        isMock1 = false;
+        isMock2 = true;
+        isMock3 = false;
+    }
+})
+
+rest3.addEventListener("click", function() {
+    if (!isMock3) {
+        mock1.style.display = "none";
+        mock2.style.display = "none";
+        mock3.style.display = "block";
+        isMock1 = false;
+        isMock2 = false;
+        isMock3 = true;
+    }
+})
+
