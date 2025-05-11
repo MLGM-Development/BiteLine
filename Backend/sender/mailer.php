a84706f908e092c65536b9542d73b05367920450
Matteo Carpentieri <matteo07.carpentieri@gmail.com>
2025-05-11 22:54:39 +0200

    Fix logo BiteLine

--- Backend/sender/mailer.php
+++ Backend/sender/mailer.php
@@ -5,8 +5,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/../../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+
 
 $mail = new PHPMailer(true);
 $mail->isSMTP();
